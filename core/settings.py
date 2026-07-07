44b6c0b948c2760d78f39efe028870bf772b821a
Inderpreet <inderpreet.singh0511@gmail.com>
2026-07-07 16:04:17 +0530

    fix static file

--- core/settings.py
+++ core/settings.py
@@ -188,7 +188,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 if DEBUG:
     MEDIA_URL = 'media/'
     MEDIA_ROOT = BASE_DIR / 'media'
