42432276820f093449428d30e93b4e57394b485d
Inderpreet <inderpreet.singh0511@gmail.com>
2026-07-07 16:05:47 +0530

    fix static file

--- core/settings.py
+++ core/settings.py
@@ -184,12 +184,14 @@ RAZORPAY_SECRET=os.getenv('RAZORPAY_SECRET')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 if DEBUG:
-    MEDIA_URL = 'media/'
+    MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
